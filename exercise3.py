5da667405e255fc34036d1f5d2c7c9d84ef6a928
Ayushman Tiwari <ayushman28at@gmail.com>
2026-04-18 14:40:07 +0530

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,127 @@
+print("============Hello===========")
+
+name = input("Please enter your name in capital:  ")
+
+print("=============Welcome to KBC",name,"================")
+print("If you want to play KBC please press ==== ST  ====")
+ST=input()
+if ST in ST:
+    print("Thanku for you choise")
+    print("Here we play KBC you have 4 question and chanse to win 100000")
+    print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    print("=============  YOUR FIRST QUESTION   ============")
+    print("1. Geography\n")
+    print("Question: Which country is known as the 'Land of the Rising Sun?")
+    list = [  "A) China","B) Japan" "C) South Korea\ ""D) Thailand" ]
+
+
+
+    print(list,"\n")
+    print("Enter option ALPHABET")
+    B  = input("Enter the answer : ")
+    if B in B:
+        print("========CONGERACULATION==========\n coerrect answer")
+    else:
+        print("=========WRONG===========\n Beter luck next time  ")
+
+
+
+
+
+
+
+
+
+        
+        
+    print("Now next question")
+    print("2. Biology\n")
+    print("Question: What color is a giraffe’s tongue? ")
+    list = ["A) Pink" "B) Black" " C) Purple" " D) Blue"]
+    print(list,"\n")
+
+    print("Enter option ALPHABET")
+    C  = input("Enter the answer : ")
+
+    if C in C:
+          print("========CONGERACULATION==========\n coerrect answer")
+    else:
+        print("=========WRONG===========\n Beter luck next time  ")
+    print("After this question you won 5000")
+
+
+
+
+
+
+
+    print("Now next question")
+    print("3. Space Science")
+    print("Question: Which planet in our solar system is known as the Red Planet? ")
+    list = ["A) Jupiter" ,"B) Venus","C) Mars" ," D) Saturn "]
+    print(list,"\n")
+
+    print("Enter option ALPHABET")
+    C  = input("Enter the answer : ")
+
+    if C in C:
+          print("========CONGERACULATION==========\n coerrect answer")
+    else:
+        print("=========WRONG===========\n Beter luck next time  ")
+
+
+
+
+
+
+    
+    print("Now ready for your final question to won   This KBC    ")
+    print("4. Astronomy/Oceanography")
+    print("Question: By what name is the killer whale more commonly known?")
+    list = ["A) Beluga","B) Orca ","C) Sperm Whale","D) Humpback" ]
+    print(list,"\n")
+
+
+    print("Enter option ALPHABET")
+    B  = input("Enter the answer : ")
+    if B in B:
+          print("========CONGERACULATION==========\n coerrect answer")
+    else:
+        print("=========WRONG===========\n Beter luck next time  ")
+
+
+
+
+
+
+
+
+    print("Thanku for comming")
+
+
+    
+
+
+
+
+
+else:
+    print("Thanku for your choise when you want to play again press run button")
